90389a3055be33fbd20fb7ab519f3a6b03c2404b
Peter Ke <peterke0911@gmail.com>
2019-07-24 21:03:44 -0700

    Improve board - readjust position, resize based on screen

--- lib/components/game_board.dart
+++ lib/components/game_board.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:connect_5/controllers/game_controller.dart';
 import 'package:connect_5/models/game.dart';
@@ -13,9 +15,13 @@ class GameBoard extends StatefulWidget {
 class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
   static const double MAX_ZOOM = 4;
   static const double MIN_ZOOM = 1;
-  static const double DEFAULT_BOARD_SIZE = 300; // TODO: adjust with screen size
 
   static const ANIMATION_DURATION = Duration(milliseconds: 150);
+
+  double get defaultBoardSize {
+    final size = MediaQuery.of(context).size;
+    return math.min(size.width, size.height) * 0.8;
+  }
   
   GameController get _controller => Provider.of<GameController>(context);
 
@@ -34,15 +40,20 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
   /// Current zoom of the board
   double _zoom = 1;
 
-  // Board zoom animations
-  Animation<double> _boardZoomAnimation;
-  AnimationController _boardZoomAnimationController;
+  // Zoom animations
+  Animation<double> _zoomAnimation;
+  AnimationController _zoomAnimationController;
+
+  // Offset animations
+  Animation<Offset> _offsetAnimation;
+  AnimationController _offsetAnimationController;
 
   @override
   void initState() {
     super.initState();
 
-    _boardZoomAnimationController = AnimationController(duration: ANIMATION_DURATION, vsync: this);
+    _zoomAnimationController = AnimationController(duration: ANIMATION_DURATION, vsync: this);
+    _offsetAnimationController = AnimationController(duration: ANIMATION_DURATION, vsync: this);
   }
 
   @override
@@ -64,12 +75,15 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
     _controller.moveBoard();
 
     setState(() {
+      // Moving board, stop offset animation
+      _offsetAnimationController.stop();
+
       if (scale.scale == 1) {
         // Single finger zoom, only adjust center (reset zoom)
         _initialZoom = _zoom;
       } else {
-        // Scaling with two fingers, stop animation
-        _boardZoomAnimationController.stop();
+        // Scaling with two fingers, stop zoom animation
+        _zoomAnimationController.stop();
         
         // Adjust zoom based on scale
         _zoom = _initialZoom * scale.scale;
@@ -91,23 +105,49 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
     } else if (_zoom < MIN_ZOOM) {
       _animateZoom(MIN_ZOOM);
     }
+
+    // Animate offset if board is too far from center
+    final halfBoardSize = defaultBoardSize * _zoom / 2;
+    final dx = math.max(-halfBoardSize, math.min(halfBoardSize, _offset.dx));
+    final dy = math.max(-halfBoardSize, math.min(halfBoardSize, _offset.dy));
+
+    if (dx != _offset.dx || dy != _offset.dy) {
+      _animateOffset(Offset(dx, dy));
+    }
   }
 
   void _animateZoom(double newZoom) {
     // Reset ongoing animation, if needed
-    _boardZoomAnimation?.removeListener(_handleZoomAnimate);
-    _boardZoomAnimationController.reset();
+    _zoomAnimation?.removeListener(_handleZoomAnimate);
+    _zoomAnimationController.reset();
 
     // Start new zoom animation
-    _boardZoomAnimation = Tween(begin: _zoom, end: newZoom)
-      .animate(_boardZoomAnimationController)
+    _zoomAnimation = Tween(begin: _zoom, end: newZoom)
+      .animate(_zoomAnimationController)
       ..addListener(_handleZoomAnimate);
 
-    _boardZoomAnimationController.forward();
+    _zoomAnimationController.forward();
   }
 
   void _handleZoomAnimate() {
-    setState(() => _zoom = _boardZoomAnimation.value);
+    setState(() => _zoom = _zoomAnimation.value);
+  }
+
+  void _animateOffset(Offset newOffset) {
+    // Remove ongoing animation, if needed
+    _offsetAnimation?.removeListener(_handleOffsetAnimate);
+    _offsetAnimationController.reset();
+
+    // Start new offset animation
+    _offsetAnimation = Tween(begin: _offset, end: newOffset)
+      .animate(_offsetAnimationController)
+      ..addListener(_handleOffsetAnimate);
+    
+    _offsetAnimationController.forward();
+  }
+
+  void _handleOffsetAnimate() {
+    setState(() => _offset = _offsetAnimation.value);
   }
 
   void _handleTapUp(TapUpDetails tap) {
@@ -119,7 +159,7 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
 
   /// Get the point on board for position, if valid. Otherwise returns null
   Point _getPoint(Offset position) {
-    final boxSize = DEFAULT_BOARD_SIZE / (_controller.game.board.size + 1);
+    final boxSize = defaultBoardSize / (_controller.game.board.size + 1);
     final x = (position.dx / boxSize).round() - 1; // -1 to compensate for padding
     final y = (position.dy / boxSize).round() - 1;
 
@@ -145,12 +185,12 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
               ..scale(_zoom)
               ..translate(_offset.dx / _zoom, _offset.dy / _zoom)
               // Position board center
-              ..translate((1 / _zoom - 1) * DEFAULT_BOARD_SIZE / 2, (1 / _zoom - 1) * DEFAULT_BOARD_SIZE / 2),
+              ..translate((1 / _zoom - 1) * defaultBoardSize / 2, (1 / _zoom - 1) * defaultBoardSize / 2),
             child: GestureDetector(
               onTapUp: _handleTapUp,
               child: Container(
-                height: DEFAULT_BOARD_SIZE,
-                width: DEFAULT_BOARD_SIZE,
+                height: defaultBoardSize,
+                width: defaultBoardSize,
                 child: CustomPaint(
                   painter: BoardPainter(
                     game: _controller.game,
